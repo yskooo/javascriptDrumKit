be6778772b5708fc4ef041aab6bbb4f74453ab01
yskooo <90546802+yskooo@users.noreply.github.com>
2022-02-21 21:15:24 +0800

    fixed bugs

--- script.js
+++ script.js
@@ -16,3 +16,5 @@ function playSound(e) {
 const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
+
+//haha di q gets
